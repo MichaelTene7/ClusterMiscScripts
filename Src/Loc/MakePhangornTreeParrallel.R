2eee871f9e78b29bf456be547fbbade1aea0687d
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2024-02-14 19:22:23 -0500

    add UNicornsFilter

--- Src/Loc/MakePhangornTreeParrallel.R
+++ Src/Loc/MakePhangornTreeParrallel.R
@@ -5,6 +5,7 @@ source("Src/Reu/cmdArgImport.R")
 
 args = c('i=c(1:10)', 'a=DemoData/UNICORNs_alignments/alignments', 'o=Output/PhangornTestOut', 't=Data/zoonomiaMasterTreeScientific.tree', 's=F', 'd=T') #This is a debug argument set. It is used to set arguments locally, when not running the code through a bash script.
 
+args = c('i=c(1:184000)', 'a=DemoData/UNICORNs_alignments/alignments', 'o=Output/UnicornsOverlap', 't=Data/zoonomiaMasterTreeScientific.tree', 's=T', 'd=T', 'f=Data/Dev/unicornsFilter.rds') #This is a debug argument set. It is used to set arguments locally, when not running the code through a bash script.
 
 
 args = c('i=c(1:10)', 'a=/share/ceph/scratch/mit221/10934053/Alignments/alignments', 'o=Output/UNICORNsTreeFiles', 't=Data/zoonomiaMasterTreeScientific.tree', 's=F', 'd=T') #This is a debug argument set. It is used to set arguments locally, when not running the code through a bash script.
@@ -16,11 +17,11 @@ outputDirectory = cmdArgImport('o')
 masterTree = cmdArgImport('t')
 bySingles = cmdArgImport('s')
 isDNA = cmdArgImport('d')
+filterFile = cmdArgImport('f')
 
 if(!dir.exists(outputDirectory)){                                      #Make output directory if it does not exist
   dir.create(outputDirectory)
 }
-
 
 if(isDNA){
   typeVal = "DNA"
@@ -30,11 +31,19 @@ if(isDNA){
   modelVal = "LG"
 }
 
+if(!is.na(filterFile)){
+  filterString = readRDS(filterFile)
+}
+
 if(bySingles){
+  fileList = list.files(alignmentDirectory)
   for(i in  fileNumber){
     print(i)
-    fileList = list.files(alignmentDirectory)
     currentAlignment = fileList[i]
+    if(!is.na(filterFile)){
+      includedFiles = 0
+      if(!currentAlignment %in% filterString){message("skip"); next}else(message(currentAlignment); includedFiles = includedFiles+1)
+    }
     alignmentFile = paste(alignmentDirectory, "/", currentAlignment, sep="")
     treeObject <- estimatePhangornTree(alnfile = alignmentFile, treefile = masterTree, submodel = modelVal, type = typeVal, format = "fasta", k = 4)
     
@@ -42,6 +51,7 @@ if(bySingles){
     outFileName = paste(outputDirectory, "/", elementName, ".tree", sep="") 
     treeString = write.tree(treeObject$tree, outFileName)
   }
+  if(!is.na(filterFile)){message(paste("Complete. Number of files created: ", includedFiles))}
 }else{
   estimatePhangornTreeAll(alndir = alignmentDirectory, treefile = masterTree, output.file = paste(outputDirectory, "AllTree.tree"), type = typeVal, submodel = modelVal)
 }
