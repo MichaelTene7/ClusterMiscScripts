532b8ba2070ea82a5ed21db91a5615e78f4f5a71
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2024-10-18 19:26:39 -0400

    add sol paths to readTrees

--- Src/Loc/runReadTrees.R
+++ Src/Loc/runReadTrees.R
@@ -1,3 +1,4 @@
+.libPaths("/share/ceph/wym219group/shared/libraries/R4")
 library(RERconverge)
 mainTrees = readTrees("Data/zoonomiaAllMammalsTrees.tsv")
 saveRDS(mainTrees, "OUtput/zoonomiaAllMammalsTrees.rds")
