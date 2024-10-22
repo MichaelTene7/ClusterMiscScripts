342c16fd03b251eb6048602e436e174408d44942
MichaelTene7 <110479175+MichaelTene7@users.noreply.github.com>
2024-10-22 19:46:47 -0400

    give maintree to readTrees with branch resetimation

--- Src/Loc/runReadTrees.R
+++ Src/Loc/runReadTrees.R
@@ -1,4 +1,4 @@
 .libPaths("/share/ceph/wym219group/shared/libraries/R4")
 library(RERconverge)
-mainTrees = readTrees("Output/newZoonomiaAlignmentSpecies.tsv")
+mainTrees = readTrees("Output/newZoonomiaAlignmentSpecies.tsv", masterTree = "/share/ceph/wym219group/shared/projects/MammalDiet/Zoonomia/NewZoonomiaMasterTreePrunedToAlignmentSpecies.nwk", reestimateBranches = T)
 saveRDS(mainTrees, "Output/zoonomiaAllMammalsTrees.rds")
